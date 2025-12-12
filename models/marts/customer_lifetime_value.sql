9d7178a76f917318e6fe95e05176a2196b990cdb
Jeff Bluemel <jeffbluemel@gmail.com>
2025-12-11 21:49:33 -0600

    add average_winning_bid to customer lifetime value

--- models/marts/customer_lifetime_value.sql
+++ models/marts/customer_lifetime_value.sql
@@ -8,7 +8,12 @@ select
     c.state,
     count(b.bid_id) as total_bids,
     sum(case when b.is_winning_bid then 1 else 0 end) as winning_bids,
-    sum(case when b.is_winning_bid then b.bid_amount else 0 end) as total_spent
+    sum(case when b.is_winning_bid then b.bid_amount else 0 end) as total_spent,
+    case 
+        when sum(case when b.is_winning_bid then 1 else 0 end) > 0 
+        then sum(case when b.is_winning_bid then b.bid_amount else 0 end) / sum(case when b.is_winning_bid then 1 else 0 end)
+        else 0 
+    end as average_winning_bid
 from {{ ref('stg_customers') }} c
 left join {{ ref('stg_bids') }} b
     on c.customer_id = b.customer_id
